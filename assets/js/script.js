16c4589e7c55d8331f9e43c48728d06fdbdeade5
aframuz <aframuz@tutanota.io>
2022-04-10 01:16:37 -0400

    Added navbar shadow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
 $(function () {
+   // Toggle nav shadow
+   $(window).scroll(function () {
+      if ($(window).scrollTop() > 10) {
+         $(".navbar").addClass("floatingNav")
+      } else {
+         $(".navbar").removeClass("floatingNav")
+      }
+   })
+
+   // Nav items scroll
    $("a").click(function () {
       var hash = this.hash
 
