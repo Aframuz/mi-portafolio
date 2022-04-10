c455acb97fdac6f8eb31f6554295a8c769cf2eda
aframuz <aframuz@tutanota.io>
2022-04-10 01:03:15 -0400

    Added JQuery 3.6.x, replaced Bootstrap smoothscroll with JQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,12 @@
-// Test
-console.log("linked")
+$(function () {
+   $("a").click(function () {
+      var hash = this.hash
+
+      $("html, body").animate(
+         {
+            scrollTop: $(hash).offset().top - 60,
+         },
+         800
+      )
+   })
+})
